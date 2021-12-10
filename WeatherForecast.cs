b50764e17f055b981eda09c331b7cce1cc5153de
kacperkapusciak <kapusciak.k@gmail.com>
2021-12-10 22:42:27 +0100

    Initial commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace learn_chords_with_me;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
